3a3dd31e7536e2d51cc5f70e90e291fbba785d0a
Rajdeep Ray <rayrajdeep2001@gmail.com>
2020-11-07 01:01:17 +0530

    Update | Launch Schedule

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const cheerio = require('cheerio');
 
 const url = "https://www.rocketlaunch.live/";
 
+var rocket_schedules = [];
+
 (async () => {
     for (let i = 1; i < 8; i++) {
         await axios.get(url + '?page=' + i)
             .then((res) => {
                 let $ = cheerio.load(res.data);
                 $('div[id=upcoming_launches_header]').remove()
-                let launch_list=[]
+                let rocket_details = []
                 let mission_vehicle = {}
                 $('h4').each((i, e) => {
                     if (i % 2 == 0) {
@@ -17,14 +19,24 @@ const url = "https://www.rocketlaunch.live/";
                     }
                     else {
                         mission_vehicle['vehicle'] = $(e).text().replace('\n', '');
-                        launch_list.push(mission_vehicle);
-                        mission_vehicle={};
+                        rocket_details.push(mission_vehicle);
+                        mission_vehicle = {};
                     }
                 })
-                
-                console.log(launch_list);
+
+                let schedule_list = [];
+                $('div[class="launch_date rlt_date"]').each((i, e) => {
+                    schedule_list.push($(e).text());
+                })
+
+                for (let i = 0; i < rocket_details.length; i++) {
+                    rocket_details[i]['schedule'] = schedule_list[i];
+                    rocket_schedules.push(rocket_details[i])
+                }
+                // console.log(rocket_details);
             }).catch((err) => {
                 console.log(err)
             });
     }
+    console.log(rocket_schedules);
 })();
